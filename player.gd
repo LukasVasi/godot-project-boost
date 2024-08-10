52be11c09a7d92732cca3c65aeb4f50ba5f4b4b5
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-08-10 17:54:01 +0300

    Added custom input actions for movement

--- player.gd
+++ player.gd
@@ -2,16 +2,16 @@ class_name Player
 extends RigidBody3D
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		var local_up: Vector3 = basis.y
 		apply_central_force(local_up * delta * 1000)
 	
 	var torque_vector: Vector3 = Vector3.ZERO
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotate_left"):
 		torque_vector += Vector3.FORWARD
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("rotate_right"):
 		torque_vector += Vector3.BACK
 		
 	apply_torque(torque_vector * delta * 100)
