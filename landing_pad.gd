16106dcdb5e9a545828ba8aae6bc009aca4289d7
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-08-10 19:22:17 +0300

    Added collision detection and additional level prototypes

---
+++ landing_pad.gd
@@ -0,0 +1,5 @@
+class_name LandingPad
+extends CSGBox3D
+
+## The file path to the scene containing the next level
+@export_file("*.tscn") var next_level_path
--- player.gd
+++ player.gd
@@ -1,10 +1,16 @@
 class_name Player
 extends RigidBody3D
 
+## The magnitude of the force applied when boosting
+@export var boost_force: float = 1000
+
+## The magnituted of the torque applied when rotating
+@export var rotation_torque: float = 100
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		var local_up: Vector3 = basis.y
-		apply_central_force(local_up * delta * 1000)
+		apply_central_force(local_up * delta * boost_force)
 	
 	var torque_vector: Vector3 = Vector3.ZERO
 	
@@ -14,4 +20,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("rotate_right"):
 		torque_vector += Vector3.BACK
 		
-	apply_torque(torque_vector * delta * 100)
+	apply_torque(torque_vector * delta * rotation_torque)
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Goal"):
+		# Check if there is a next level
+		if "next_level_path" in body and body.next_level_path:
+			print("Level complete!")
+			_complete_level(body.next_level_path)
+		else:
+			print("You win!")
+		
+	if body.is_in_group("Hazard"):
+		_handle_crash()
+
+func _handle_crash() -> void:
+	print("You crashed")
+	get_tree().reload_current_scene()
+	
+func _complete_level(next_level_path: String) -> void:
+	get_tree().change_scene_to_file(next_level_path)
